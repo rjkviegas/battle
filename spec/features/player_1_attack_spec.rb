9d379f607950608cf177684a48cdf30de9aad09d
DillonBarker <barker.dill98@gmail.com>
2020-07-22 17:44:20 +0100

    harry lb update

--- spec/features/player_1_attack_spec.rb
+++ spec/features/player_1_attack_spec.rb
@@ -2,6 +2,6 @@ feature 'player 1 attack confirmation' do
     scenario 'confirmation of attack displayed' do
         sign_in_and_play
         click_button "Attack!"
-        expect(page).to have_content 'Harry P attacked Voldie M! How unusual of him.'
+        expect(page).to have_content 'Harry P musted all his puny strength and attacked Voldie M! How unusual of him.'
     end
 end
