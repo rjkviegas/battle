9e26fe6c3114431d83fdbb9396fb4c5975510ca7
Ryan Viegas <rjkviegas@gmail.com>
2020-07-23 18:44:41 +0100

    extracted attack method and tests from game

--- lib/game.rb
+++ lib/game.rb
@@ -5,10 +5,6 @@ attr_reader :current_turn
         @current_turn = player_1
     end
 
-    def attack(player)
-        player.receive_damage
-    end
-
     def player_1
         @players.first
     end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,13 +4,7 @@ describe Game do
     subject(:game) { described_class.new(player_1, player_2) }
     let(:player_1) { double :player }
     let(:player_2) { double :player }
-    
-    describe '#attack' do
-        it 'reduces hp of player' do
-            expect(player_2).to receive(:receive_damage)
-            game.attack(player_2)
-        end
-    end
+
 
     describe '#player_1' do
         it 'retrieves the first player' do
